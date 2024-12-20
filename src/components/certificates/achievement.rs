757267e59d1648be8968990a203f96aa718245f8
Christian M <chriamue@gmail.com>
2024-12-20 13:15:16 +0100

    :lipstick: styles achievement

--- src/components/certificates/achievement.rs
+++ src/components/certificates/achievement.rs
@@ -33,3 +33,34 @@ pub fn achievement_component(props: &AchievementComponentProps) -> Html {
         </div>
     }
 }
+
+#[cfg(feature = "yew-preview")]
+mod preview {
+    use super::*;
+    use yew_preview::prelude::*;
+
+    yew_preview::create_preview!(
+        AchievementComponent,
+        AchievementComponentProps {
+            achievement: AchievementDefinition {
+                name: "Achievement Name".to_string(),
+                description: "Achievement Description".to_string(),
+                icon: "fa-award".to_string(),
+                condition: "".to_string(),
+                id: "achievement-id".to_string(),
+            },
+        },
+        (
+            "condition not met",
+            AchievementComponentProps {
+                achievement: AchievementDefinition {
+                    name: "Achievement".to_string(),
+                    description: "Achievement with unmet condition".to_string(),
+                    icon: "fa-praying-hands".to_string(),
+                    condition: "false".to_string(),
+                    id: "achievement-id".to_string(),
+                },
+            }
+        ),
+    );
+}
