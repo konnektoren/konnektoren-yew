677fd12079c9a64aa818bb3aa7e2b2d9310ea734
Christian M <chriamue@gmail.com>
2025-01-31 08:17:23 +0100

    :goal_net: catch errors in session provider

--- src/providers/session_provider.rs
+++ src/providers/session_provider.rs
@@ -28,22 +28,49 @@ impl PartialEq for SessionProviderProps {
 #[function_component(SessionProvider)]
 pub fn session_provider(props: &SessionProviderProps) -> Html {
     let session_initializer = props.session_initializer.clone();
-    let session = use_state(|| session_initializer.initialize(&Session::default()).unwrap());
+    let error = use_state(|| None::<String>);
+
+    let session = use_state(
+        || match session_initializer.initialize(&Session::default()) {
+            Ok(session) => session,
+            Err(e) => {
+                log::error!("Failed to initialize session: {:?}", e);
+                Session::default()
+            }
+        },
+    );
 
     // Load session
     {
         let session = session.clone();
+        let error = error.clone();
         let session_repository = props.session_repository.clone();
         let session_initializer = session_initializer.clone();
 
         use_effect_with((), move |_| {
             wasm_bindgen_futures::spawn_local(async move {
-                if let Ok(Some(loaded_session)) =
-                    session_repository.get_session(SESSION_STORAGE_KEY).await
-                {
-                    let initialized_session =
-                        session_initializer.initialize(&loaded_session).unwrap();
-                    session.set(initialized_session);
+                match session_repository.get_session(SESSION_STORAGE_KEY).await {
+                    Ok(Some(loaded_session)) => {
+                        match session_initializer.initialize(&loaded_session) {
+                            Ok(initialized_session) => {
+                                session.set(initialized_session);
+                                error.set(None);
+                            }
+                            Err(e) => {
+                                let err_msg = format!("Failed to initialize session: {:?}", e);
+                                log::error!("{}", err_msg);
+                                error.set(Some(err_msg));
+                            }
+                        }
+                    }
+                    Ok(None) => {
+                        log::info!("No existing session found");
+                    }
+                    Err(e) => {
+                        let err_msg = format!("Failed to load session: {:?}", e);
+                        log::error!("{}", err_msg);
+                        error.set(Some(err_msg));
+                    }
                 }
             });
             || ()
@@ -53,6 +80,7 @@ pub fn session_provider(props: &SessionProviderProps) -> Html {
     {
         let session_repository = props.session_repository.clone();
         let session = session.clone();
+        let error = error.clone();
 
         use_effect_with(session.clone(), move |_| {
             let session = session.clone();
@@ -62,7 +90,9 @@ pub fn session_provider(props: &SessionProviderProps) -> Html {
                     .update_session(SESSION_STORAGE_KEY, &session)
                     .await
                 {
-                    log::error!("Failed to save session: {:?}", e);
+                    let err_msg = format!("Failed to save session: {:?}", e);
+                    log::error!("{}", err_msg);
+                    error.set(Some(err_msg));
                 }
             });
             || ()
@@ -73,6 +103,12 @@ pub fn session_provider(props: &SessionProviderProps) -> Html {
 
     html! {
         <ContextProvider<SessionContext> {context}>
+            if let Some(err_msg) = (*error).clone() {
+                <div class="error-message" style="color: red; padding: 1em; margin: 1em; border: 1px solid red;">
+                    <h3>{"Session Error"}</h3>
+                    <p>{err_msg}</p>
+                </div>
+            }
             { props.children.clone() }
         </ContextProvider<SessionContext>>
     }
