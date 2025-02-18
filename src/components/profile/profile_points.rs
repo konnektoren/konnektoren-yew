f170e43c4eae58e78e3d536bfbb0ea97afff45ea
Christian M <chriamue@gmail.com>
2025-02-18 19:41:59 +0100

    :lipstick: improved profile points style

--- src/components/profile/profile_points.rs
+++ src/components/profile/profile_points.rs
@@ -1,4 +1,5 @@
 use konnektoren_core::prelude::PlayerProfile;
+use web_sys::MouseEvent;
 use yew::prelude::*;
 
 #[derive(Properties, PartialEq, Clone)]
@@ -8,13 +9,37 @@ pub struct ProfilePointsProps {
 
 #[function_component(ProfilePointsComponent)]
 pub fn profile_points_component(props: &ProfilePointsProps) -> Html {
+    let expanded = use_state(|| false);
     let points = props.profile.xp;
+    let first_letter = props.profile.name.chars().next().unwrap_or('?');
+
+    let toggle_expanded = {
+        let expanded = expanded.clone();
+        Callback::from(move |e: MouseEvent| {
+            e.prevent_default();
+            expanded.set(!*expanded);
+        })
+    };
+
+    let class = classes!(
+        "profile-points",
+        expanded.then(|| "profile-points--expanded")
+    );
 
     html! {
-        <div class="profile-points">
-            <div class="profile-points__icon">{"⭐️"}</div>
-            <div class="profile-points__name">{ &props.profile.name }</div>
-            <div class="profile-points__points">{ points }</div>
+        <div {class} onclick={toggle_expanded}>
+            <div class="profile-points__badge">
+                <div class="profile-points__badge-top">
+                    <span class="profile-points__icon">{"⭐️"}</span>
+                    <span class="profile-points__initial">{ first_letter }</span>
+                </div>
+                <div class="profile-points__points">{ points }</div>
+            </div>
+            <div class="profile-points__expanded">
+                <span class="profile-points__icon">{"⭐️"}</span>
+                <span class="profile-points__points">{ points }</span>
+                <span class="profile-points__name">{ &props.profile.name }</span>
+            </div>
         </div>
     }
 }
